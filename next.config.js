c3bafa99305d1fca6402d4e09888f1e0bf72f055
techiabhil <techiabhil@gmail.com>
2022-12-19 18:17:55 +0530

    Fix: updated base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: 'http://148.66.142.19:5000/api/v1',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
