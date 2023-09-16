450d4e872e7d93348a2100a2ed9c3eb8756bb9fb
techiabhil <techiabhil@gmail.com>
2023-09-16 16:34:56 +0530

    Feat: payment details added

--- pages/addresses/index.tsx
+++ pages/addresses/index.tsx
@@ -1,11 +1,27 @@
 import axios from 'axios';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import CustomLoader from '../../components/common/CustomLoader';
 import Layout from '../../components/layout/layout';
 import { useAppSelector } from '../../store/store';
 
+function loadScript(src: string) {
+    return new Promise((resolve) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = () => {
+            resolve(true);
+        };
+        script.onerror = () => {
+            resolve(false);
+        };
+        document.body.appendChild(script);
+    });
+}
+
 const Addresses = () => {
+    const router = useRouter();
     const [addresses, setAddresses] = useState<any[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [selectedAddress, setSelectedAddress] = useState<any>();
@@ -17,10 +33,19 @@ const Addresses = () => {
             return total + item?.ct_price * item?.ct_qty;
         }, 0);
     }, [cartItems]);
+    console.log('subTotal = ', subTotal);
     useEffect(() => {
         (async () => {
             try {
                 setIsLoading(true);
+                const res = await loadScript(
+                    'https://checkout.razorpay.com/v1/checkout.js'
+                );
+
+                if (!res) {
+                    alert('Razropay failed to load!!');
+                    return;
+                }
                 const { addresses }: any = await axios.get(`/user/address`);
                 if (Array.isArray(addresses)) {
                     const primaryAddress = addresses.find(
@@ -41,34 +66,72 @@ const Addresses = () => {
     const [isOrderInfoFetching, setIsOrderInfoFetching] =
         useState<boolean>(false);
 
-    const initiatePayment = useCallback(
-        (address: string = '') => {
-            const paymentOptions = {
-                key: 'YOUR_KEY_ID', // Enter the Key ID generated from the Dashboard
-                amount: `${subTotal}`, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-                currency: 'INR',
-                name: 'PetsNMore', //your business name
-                description: 'Test Transaction',
-                order_id: 'order_9A33XWu170gUtm', //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-                handler: function (response: any) {
-                    alert(response.razorpay_payment_id);
-                    alert(response.razorpay_order_id);
-                    alert(response.razorpay_signature);
-                },
-                prefill: {
-                    name: user.us_full_name, //your customer's name
-                    email: user.us_email,
-                    contact: user.us_phone, //Provide the customer's phone number for better conversion rates
-                },
-                notes: {
-                    address: address,
-                },
-                theme: {
-                    color: '#3399cc',
-                },
-            };
+    const handlePaymentSuccess = useCallback(
+        async (
+            { razorpay_payment_id, razorpay_signature }: any,
+            orderID: string
+        ) => {
+            try {
+                setIsLoading(true);
+                await axios.post('/marketplace/cart/checkout/payment', {
+                    order_id: +orderID,
+                    payment_id: razorpay_payment_id,
+                    payment_signature: razorpay_signature,
+                });
+                setIsLoading(false);
+                router.push(`/order-details/${orderID}`);
+            } catch (e) {
+                setIsLoading(false);
+                alert('Something went wrong while saving payment info!');
+            }
         },
-        [subTotal, user.us_email, user.us_full_name, user.us_phone]
+        [router]
+    );
+
+    const initiatePayment = useCallback(
+        async (
+            orderDetails: any,
+            tbl_order_id: string,
+            address: string = ''
+        ) => {
+            try {
+                const paymentOptions = {
+                    key: process.env.NEXT_PUBLIC_RAZOR_PAY_KEY_ID, // Enter the Key ID generated from the Dashboard
+                    amount: `${subTotal}`, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+                    currency: orderDetails.currency,
+                    name: 'PetsNMore', //your business name
+                    description: 'Order Transaction',
+                    order_id: orderDetails.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+                    handler: function (response: any) {
+                        handlePaymentSuccess(response, tbl_order_id);
+                    },
+                    prefill: {
+                        name: user.us_full_name, //your customer's name
+                        email: user.us_email,
+                        contact: user.us_phone, //Provide the customer's phone number for better conversion rates
+                    },
+                    notes: {
+                        address: address,
+                    },
+                    theme: {
+                        hide_topbar: true,
+                        color: '#3399cc',
+                    },
+                };
+                // @ts-ignore
+                const paymentObject = new window.Razorpay(paymentOptions);
+                paymentObject.open();
+            } catch (e) {
+                alert('Something went wrong at payment!');
+            }
+        },
+        [
+            handlePaymentSuccess,
+            subTotal,
+            user.us_email,
+            user.us_full_name,
+            user.us_phone,
+        ]
     );
 
     const getPaymentDetails = useCallback(async () => {
@@ -94,8 +157,7 @@ const Addresses = () => {
                 cart_items: cartItemIds,
             });
             if (result.message === 'Success') {
-                initiatePayment(address);
-                alert('Redircet to payment gateway pending implementation');
+                initiatePayment(result?.rp_order, result?.order_id, address);
             }
             setIsOrderInfoFetching(false);
         } catch (e) {
@@ -109,10 +171,6 @@ const Addresses = () => {
         <Layout>
             <Head>
                 <title>Address</title>
-                <script
-                    src="https://checkout.razorpay.com/v1/checkout.js"
-                    async
-                ></script>
             </Head>
             <section className="mt-1 mb-1 container">
                 <div className="mt-5 row d-flex flex-column align-items-center justify-content-center section-text-style">
@@ -125,8 +183,8 @@ const Addresses = () => {
                 <CustomLoader show={isLoading || isOrderInfoFetching} />
                 <div className="col-center my-2">
                     {addresses.length > 0 &&
-                        addresses.map(
-                            ({
+                        addresses.map((addressItem: any) => {
+                            const {
                                 ad_id,
                                 ad_title,
                                 ad_zip,
@@ -134,42 +192,46 @@ const Addresses = () => {
                                 ad_state,
                                 ad_city,
                                 ad_address,
-                            }) => {
-                                return (
-                                    <div
-                                        key={ad_id}
-                                        className="bottom-ruler col-md-6 col-sm-12 mt-2"
-                                        role="button"
-                                        onClick={() =>
-                                            setSelectedAddress(ad_id ?? '')
-                                        }
-                                    >
-                                        <div className="form-check">
-                                            <input
-                                                className="form-check-input"
-                                                type="radio"
-                                                name="addressRadio"
-                                                id={`addressRadio-${ad_id}`}
-                                                value={ad_id}
-                                                checked={
-                                                    selectedAddress?.ad_id ===
+                            } = addressItem;
+                            return (
+                                <div
+                                    key={ad_id}
+                                    className="bottom-ruler col-md-6 col-sm-12 mt-2"
+                                    role="button"
+                                    onClick={() =>
+                                        setSelectedAddress(addressItem ?? '')
+                                    }
+                                >
+                                    <div className="form-check">
+                                        <input
+                                            className="form-check-input"
+                                            type="radio"
+                                            name="addressRadio"
+                                            id={`addressRadio-${ad_id}`}
+                                            value={ad_id}
+                                            checked={
+                                                selectedAddress?.ad_id === ad_id
+                                            }
+                                            onChange={() =>
+                                                console.log(
+                                                    'selcted address = ',
                                                     ad_id
-                                                }
-                                            />
-                                            <label
-                                                className="form-check-label font-grey"
-                                                htmlFor="exampleRadios1"
-                                            >
-                                                {ad_title}
-                                            </label>
-                                        </div>
-                                        <p className="ml-4">
-                                            {`${ad_address}, ${ad_city},${ad_state}, ${ad_country}, ${ad_zip}`}
-                                        </p>
+                                                )
+                                            }
+                                        />
+                                        <label
+                                            className="form-check-label font-grey"
+                                            htmlFor="exampleRadios1"
+                                        >
+                                            {ad_title}
+                                        </label>
                                     </div>
-                                );
-                            }
-                        )}
+                                    <p className="ml-4">
+                                        {`${ad_address}, ${ad_city},${ad_state}, ${ad_country}, ${ad_zip}`}
+                                    </p>
+                                </div>
+                            );
+                        })}
                 </div>
                 {addresses?.length > 0 && (
                     <div className="row d-flex justify-content-center mt-4">
